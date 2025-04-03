eda1161b9196b5f379f02784fda77a1bc2a228e7
Pritish Kumar Senapati <pritishsenapati161@gmail.com>
2025-04-03 22:51:22 +0530

    Add files via upload
    ROLL THE DICE SIMULATOR

---
+++ PROJECT 3 .py
@@ -0,0 +1,14 @@
+# ROLL THE DICE SIMULATOR
+
+# Signing in
+
+import random
+
+def roll_dice():
+    input("Press Enter to roll the dice...")
+    print("🎲 You rolled:", random.randint(1, 6))
+
+roll_dice()
+
+# Signing off
+
